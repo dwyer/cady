89626a3ecbcee486ab9554a5c8e4514b22c19432
yashination <ayashinsky@gmail.com>
2019-07-04 17:14:59 -0700

    vector math + unit test grid

---
+++ source/vetor.js
@@ -0,0 +1,25 @@
+function vector(x_coordinate, y_coordinate) 
+{
+	this.x_coordinate = x_coordinate;
+	this.y_coordinate = y_coordinate;
+	this.norm = function(){
+		let x_squared = Math.pow(this.x_coordinate, 2);
+		let y_squared = Math.pow(this.y_coordinate, 2);
+		let result = Math.sqrt(x_squared + y_squared);
+		return result;
+	}
+	this.dot_product = function(other_vector){
+		let result_x_coordinate = this.x_coordinate * other_vector.x_coordinate;
+		let result_y_coordinate = this.y_coordinate * other_vector.y_coordinate;
+		let result = result_x_coordinate + result_y_coordinate;
+		return result;
+	}
+	this.calculate_theta = function(other_vector){
+		let numerator = this.dot_product(other_vector);
+		let this_norm = this.norm();
+		let other_norm = other_vector.norm();
+		let denominator = this_norm + other_norm;
+		let result = numerator / denominator;
+		return result;
+	}
+}
